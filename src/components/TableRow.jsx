c5534acc954f0a7677e1d517c956ac23b246d395
catborn <itnoz@yandex.ru>
2024-11-03 20:23:04 +0300

    del comment table

--- src/components/TableRow.jsx
+++ src/components/TableRow.jsx
@@ -33,15 +33,6 @@ const TableRow = ({ id, english, transcription, russian, tags }) => {
 
   return (
     <tr>
-      {/* <td>{id}</td>
-      <td>{english}</td>
-      <td>{transcription}</td>
-      <td>{russian}</td>
-      <td>{tags}</td>
-      <td>
-        <Button />
-        <Button />
-      </td> */}
       <td>{id}</td>
       {isEditing ? (
         <>
